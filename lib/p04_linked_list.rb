f0d20f496519ef1bf4ce6dd8a1e9e75e1deaf78f
Allen Chen <alchen1231@gmail.com>
2017-06-14 02:25:52 -0700

    p04 completed

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -27,36 +27,76 @@ class LinkedList
   end
 
   def [](i)
-    each_with_index { |node, j| return node if i == j }
+    cur_node = @head.next
+    idx = 0
+      loop do
+        return cur_node if idx == i
+        cur_node = cur_node.next
+        break if cur_node == @tail
+        idx += 1
+      end
     nil
   end
 
   def first
+    return nil if empty?
+    @head.next
   end
 
   def last
+    return nil if empty?
+    @tail.prev
   end
 
   def empty?
-
+    return true if @head.next == @tail
+    false
   end
 
   def get(key)
+    each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    each {|node| return true if node.key == key}
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    @tail.prev.next = new_node
+    new_node.prev = @tail.prev
+    new_node.next = @tail
+    @tail.prev = new_node
+    # new_node
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+        return node;
+      end
+    end
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.prev.next = node.next
+        node.next.prev = node.prev
+      end
+    end
   end
 
   def each
+    node = @head.next
+    until node == @tail
+      yield node
+      node = node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
