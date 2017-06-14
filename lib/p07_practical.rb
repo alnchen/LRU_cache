d48c58a34c7e0f8bb5d668bce1a897f1fc03537c
Allen Chen <alchen1231@gmail.com>
2017-06-14 03:16:34 -0700

    practical work

--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,6 +1,16 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string)
-  map = HashMap.new
+  map = HashMap.new()
   letters = string.chars
+
+  letters.each do |char|
+    map.include?(char) ? map[char] += 1 : map[char] = 1
+  end
+
+  nums = []
+  map.each { |link, val| nums << val }
+  expected_result = [1]
+  (string.length/2).times { expected_result.push(2) }
+  expected_result == nums.sort
 end
